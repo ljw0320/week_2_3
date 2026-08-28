ab7a59fac3be94f0a131dade79229ab78937ce54
ljw0320 <thewjdgy12@gmail.com>
2026-08-28 11:20:01 +0900

    01_string.py edit

--- week2/1. basic/01_string.py
+++ week2/1. basic/01_string.py
@@ -24,7 +24,61 @@
 - 문자열을 뒤집어서 비교하거나, 양 끝에서 시작해 중앙으로 이동하며 비교하세요
 """
 
-def is_palindrome(s):
+# 아이디어 : 문제를 어떻게 해결할 것인가?
+# 1. 받은 입력을 변수에 저장한다.
+# 2. 변수에서 알파벳, 숫자를 제외한 문자를 제거한다. isalnum() 메서드
+# 3. 변수를 모두 소문자로 변환한다. lower() 메서드
+# 4-1. 문자열을 뒤집는 방식을 이용해서 비교한다. [::-1] 사용
+# 4-2. 양 끝 인덱스를 이용한 투 포인터 방식을 이용한다.
+
+# 핵심 문제점 : isalnum()을 사용하면 공백이나 특수문자가 있으면 False를 반환한다
+# 주어진 문제는 특수문자나 공백이 있다면 무시해야 한다.
+# 따라서 text.isalnum()을 바로 사용하지 않고 가공해야한다.
+## 아이디어 1 
+# 문자열의 길이와 같은 배열 생성
+# 생성된 배열에 인덱스마다 문자열의 문자를 하나씩 삽입한다.
+# 각 문자에 대해 isalnum()을 사용한다. 
+# True인 배열만 모아서 문자열을 생성한다.
+# 생성된 문자열이 회문인지 검사한다.
+
+# 사전 지식
+## 1. isalnum() ##
+# python의 문자열 메서드.
+# 문자열이 오직 문자(알파벳, 한글 등)와 숫자로만 이루어져 있는지 검사.
+# 문자열.isalnum()
+# 조건 1. 문자열이 비어있지 않아야 함.
+# 조건 2. 모든 문자가 문자 또는 숫자여야 함.
+# - case - 
+# print("hello123".isalnum())   # True
+# print("hello".isalnum())      # True
+# print("12345".isalnum())      # True
+# print("안녕123".isalnum())     # True
+
+# print("hello 123".isalnum())  # False → 공백
+# print("hello!".isalnum())     # False → !
+# print("abc_123".isalnum())    # False → _
+# print("".isalnum())           # False → 빈 문자열
+
+# isalnum() 코드 내부 구조 예시
+# def isalnum_example(text):
+#     if len(text) == 0:
+#         return False
+
+#     for char in text:
+#         if not (char.isalpha() or char.isdigit()):
+#             return False
+
+#     return True
+
+## 2. lower() ##
+문자열 안의 영문 대문자를 소문자로 바꿔서 새로운 문자열을 반환
+
+def is_palindrome(s):        
+    if not (s.isalnum())        
+        if len(s) == 0: # 문자열 길이가 0이면 바로 False 반환
+            return False
+        
+
     """
     문자열이 회문인지 판별하는 함수
     
