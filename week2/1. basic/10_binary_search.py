c619f99e7aee926bbfa8fc55fb975932820b1bcc
ljw0320 <thewjdgy12@gmail.com>
2026-08-29 16:33:45 +0900

    10번 풀이 완료

--- week2/1. basic/10_binary_search.py
+++ week2/1. basic/10_binary_search.py
@@ -22,29 +22,44 @@
 - arr[mid]와 target 비교하여 범위 조정
 """
 
+"""
+이분 탐색 구현
+
+Args:
+    arr: 정렬된 배열
+    target: 찾을 값
+
+Returns:
+    target의 인덱스 (없으면 -1)
+"""
+
+# TODO: left가 right보다 작거나 같을 때까지 반복
+## 중간 인덱스 계산
+## arr[mid]와 target 비교
+## 같으면 mid 반환
+## target이 더 크면 left = mid + 1
+## target이 더 작으면 right = mid - 1
+pass
+
+# 정렬이 완료된 배열에서 목표값 찾기
+
 def binary_search(arr, target):
-    """
-    이분 탐색 구현
-    
-    Args:
-        arr: 정렬된 배열
-        target: 찾을 값
-    
-    Returns:
-        target의 인덱스 (없으면 -1)
-    """
     left = 0
-    right = len(arr) - 1
-    
-    # TODO: left가 right보다 작거나 같을 때까지 반복
-    ## 중간 인덱스 계산
-    ## arr[mid]와 target 비교
-    ## 같으면 mid 반환
-    ## target이 더 크면 left = mid + 1
-    ## target이 더 작으면 right = mid - 1
-    pass
-    
+    right = len(arr) - 1    
+
+    for i in range(right):
+        mid = int((left + right)/2)    
+        if arr[mid] == target: # arr[mid]가 target이 될 때까지 반복
+            return mid    
+        if arr[mid]>target: # 탐색범위 왼쪽 절반으로 변경
+            right = mid - 1
+        if arr[mid]<target: # 탐색범위 오른쪽 절반으로 변경
+            left = mid + 1    
     return -1
+
+# print(binary_search([1,2,3,4,5,6], 1))
+# print(binary_search([1,2,3,4,5,6], 4))
+# print(binary_search([1,2,3,4,5,6], 0))
 
 # 테스트 케이스
 if __name__ == "__main__":
