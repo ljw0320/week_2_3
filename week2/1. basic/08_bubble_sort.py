06916da5366477d04540e74b34f52ac8567db8da
ljw0320 <thewjdgy12@gmail.com>
2026-08-29 13:57:16 +0900

    8번 풀이 완료

--- week2/1. basic/08_bubble_sort.py
+++ week2/1. basic/08_bubble_sort.py
@@ -22,52 +22,91 @@
 - 최적화: 교환이 없으면 이미 정렬된 것이므로 조기 종료
 """
 
+"""
+사전지식
+1. 버블 정렬
+# 서로 이웃한 두 값을 비교해서 '순서'가 잘못되어 있으면 교환하는 작업을 반복하는 정렬 방법
+
+ex) nums = [5, 3, 4, 1]
+#1 왼쪽 두개부터 비교
+[5, 3, 4, 1]
+ ↑  ↑
+5 > 3 이므로 교환
+↓
+[3, 5, 4, 1]
+
+#2 다음 두 값 비교
+[3, 5, 4, 1]
+    ↑  ↑
+5 > 4 → 교환
+↓
+[3, 4, 5, 1]
+
+#3 다시 다음 두 값 비교
+[3, 4, 5, 1]
+       ↑  ↑
+5 > 1 → 교환
+↓
+[3, 4, 1, 5]
+"""
+
+"""
+버블 정렬 구현
+
+Args:
+    arr: 정렬할 배열
+
+Returns:
+    정렬된 배열
+"""
+
+# TODO: 외부 반복문 - n-1번 반복
+# 각 패스마다 가장 큰 원소가 끝으로 이동
+## TODO: 내부 반복문 - 인접한 원소 비교
+## 0부터 n-i-1까지 반복 (이미 정렬된 뒷부분 제외)
+## TODO: 인접한 두 원소 비교 및 교환
+## arr[j] > arr[j+1]이면 교환
+## 외부 반복문: n-1번 실행
+
 def bubble_sort(arr):
-    """
-    버블 정렬 구현
-    
-    Args:
-        arr: 정렬할 배열
-    
-    Returns:
-        정렬된 배열
-    """
-    n = len(arr)
-    
-    # TODO: 외부 반복문 - n-1번 반복
-    # 각 패스마다 가장 큰 원소가 끝으로 이동
-    ## TODO: 내부 반복문 - 인접한 원소 비교
-    ## 0부터 n-i-1까지 반복 (이미 정렬된 뒷부분 제외)
-    ## TODO: 인접한 두 원소 비교 및 교환
-    ## arr[j] > arr[j+1]이면 교환
-    ## 외부 반복문: n-1번 실행
-    pass
-        
+    n = len(arr)        
+    for i in range(n-1):  # 외부 반복문: 내부 반복문을 반복 시행
+        for j in range(n-i-1): # 내부 반복문 현재 원소가 다음것보다 크면 교환
+            if arr[j]>arr[j+1]: 
+                tempArr = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = tempArr    
     return arr
 
+"""
+최적화된 버블 정렬 (조기 종료 포함)
+
+Args:
+    arr: 정렬할 배열
+
+Returns:
+    정렬된 배열
+"""
+# TODO: 내부 반복문과 교환 로직 구현
+# 교환이 발생하면 swapped = True 설정        
+pass
+
+# TODO: 교환이 없으면 이미 정렬된 것이므로 break
+pass
+
+# 한번 더 체크
 def bubble_sort_optimized(arr):
-    """
-    최적화된 버블 정렬 (조기 종료 포함)
-    
-    Args:
-        arr: 정렬할 배열
-    
-    Returns:
-        정렬된 배열
-    """
-    n = len(arr)
-    
+    n = len(arr)    
     for i in range(n):
-        swapped = False  # 교환 발생 여부
-        
-        # TODO: 내부 반복문과 교환 로직 구현
-        # 교환이 발생하면 swapped = True 설정        
-        pass
-        
-
-        # TODO: 교환이 없으면 이미 정렬된 것이므로 break
-        pass
-
+        swapped = False  
+        for j in range(n-i):
+            if arr[j]>arr[j+1]: # 교환 발생하면 swapped = True
+                swapped = True
+                tempArr = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = tempArr
+            if swapped == False: # 교환 없으면 해당 시행에서는 정렬이 완료된 것이므로 내부 반복 종료
+                break               
     return arr
 
 # 테스트 케이스
