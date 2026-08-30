b0b5bd075ae983bfb24ccdd5760bb428adf39cc1
ljw0320 <thewjdgy12@gmail.com>
2026-08-30 16:43:04 +0900

    15번 풀이 완료

--- week2/1. basic/15_hash_table.py
+++ week2/1. basic/15_hash_table.py
@@ -2,7 +2,7 @@
 [해시 테이블 - 학생 성적 관리]
 
 참고:
-- 파이썬의 딕셔너리(dict)는 내부적으로 해시 테이블로 구현되어 있습니다.
+- 파이썬의 딕셔너리(dict)는 내부적으로 "해시 테이블"로 구현되어 있습니다.
 - 따라서 딕셔너리를 사용하면 해시 테이블의 특성을 그대로 활용할 수 있습니다.
 - week1의 01번 문제를 복기 해 보세요.
 
@@ -27,41 +27,66 @@
 힌트:
 - 딕셔너리 사용
 - 평균: sum(scores.values()) / len(scores)
-- 최고점: max(scores, key=scores.get)
+- 최고점 : max(scores.values())
+- 최고점 학생: max(scores, key=scores.get)
+: scores에서 최고 value인 key를 얻을 수 있음
+"""
+"""
+학생 성적 관리 시스템
+
+Args:
+    students: {이름: 점수} 딕셔너리
+
+Returns:
+    평균, 최고점 학생 이름, 최고점
 """
 
-def manage_grades(students):
-    """
-    학생 성적 관리 시스템
-    
-    Args:
-        students: {이름: 점수} 딕셔너리
-    
-    Returns:
-        평균, 최고점 학생 이름, 최고점
-    """
-    # TODO: 평균 점수 계산
-    pass
-    
-    
-    # TODO: 최고 점수 학생 찾기
-    pass
-    
+"""
+사전지식
+dictionary method
+1. 값 가져오기:	dict[key]
+2. 안전하게 값 가져오기(없는 key일 경우 None반환):	dict.get(key) 
+3. 모든 키 확인:	dict.keys()
+4. 모든 값 확인:	dict.values()
+5. 키와 값 같이 순회:	dict.items()
+6. 삭제:	dict.pop(key)
+7. 딕셔너리에 특정 키 있는지 확인(in): if "score" in student
+
+"""
+# TODO: 평균 점수 계산
+pass
+
+
+# TODO: 최고 점수 학생 찾기
+pass
+
+def manage_grades(students):    
+    # 입력된 dictionary에서 점수 평균, 최고점 학생, 최고점 산출
+    average = sum(students.values())/len(students)
+    top_student = max(students, key=students.get)    
+    top_score = max(students.values())
     return average, top_student, top_score
 
-def find_student_score(students, name):
-    """
-    특정 학생의 점수 조회
-    
-    Args:
-        students: 학생 딕셔너리
-        name: 찾을 학생 이름
-    
-    Returns:
-        점수 (없으면 None)
-    """
-    # TODO: students에서 name 찾기
-    pass
+"""
+특정 학생의 점수 조회
+
+Args:
+    students: 학생 딕셔너리
+    name: 찾을 학생 이름
+
+Returns:
+    점수 (없으면 None)
+"""
+# TODO: students에서 name 찾기
+pass
+
+def find_student_score(students, name):    
+    # 루프로 순회하여 딕셔너리의 key 탐색
+    # name 있으면 해당 score 반환
+    if name in students:
+        return students.get(name)        
+    # 없으면 None 반환
+    return None
 
 # 테스트 케이스
 if __name__ == "__main__":
